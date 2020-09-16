94af758e490bc318a8a8116bb2ba0e6a01b3f181
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-09-16 13:36:34 -0400

    updated layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,22 @@
 class PostsController < ApplicationController
 
-
-
-    
-
 get '/posts' do
  @posts = Post.all
 erb:'posts/index'
 end
 
+get "/posts/new" do 
+erb :"posts/new"
+end
+
+post "/posts" do
+   post= Post.create(work: params[:work], description: params[:description], link: params[:link], user_id: current_user.id)
+    redirect "/posts/#{post.id}"
+end
+
+
 get '/posts/:id' do
 @posts = Post.find(params[:id])
 erb :"/posts/show"
 end
-end 
+end
