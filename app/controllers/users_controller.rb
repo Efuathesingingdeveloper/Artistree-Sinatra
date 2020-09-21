6b2b6bbafefde131c5fe47fab65d1f9ed1a7e13e
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-09-21 14:54:27 -0400

    took away edit user funtionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,46 @@
 class UsersController < ApplicationController
  
-
+  
   get "/login" do
     erb :'users/login'
   end 
-
+  
+  
   post "/login" do
-user = User.find_by(email: params[:email])
-if user && user.authenticate(params[:password])
-session[:user_id] = user.id
-
-flash[:message] = "Welcome back #{user.name} !"
-redirect "/users/#{user.id}"
-else
-  flash[:error] ="Invaid Email or Password.Try again !"
-  redirect '/login'
-  end 
-end  
-
-get "/users/:id" do
- 
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      
+      flash[:message] = "Welcome back #{user.name} !"
+      redirect "/users/#{user.id}"
+    else
+      flash[:error] ="Invaid Email or Password.Try again !"
+      redirect '/login'
+    end 
+  end  
+  
+  get "/users/:id" do
+    
     @user = User.find_by(id: params[:id])
     erb :'users/show'
   end 
-
- get '/signup' do
-  erb :'/users/signup'
-end 
-
-post '/signup' do
-  @user = User.new(params)
-
-  if @user.save
-  session[:user_id] = @user.id
-  redirect"/users/#{@user.id}"
-  else
-    flash[:error]="Please fill out all fields to make an Artistree !"
-    redirect '/signup'
-  end
-end 
-
-get '/users/:id' do
-  @user = User.find(params[:id])
-  erb :'/users/edit'
-end
-
-get '/users/:id/edit' do
-  @user = User.find(params[:id])
-  if authorized_to_edit_user?(@user)
-   erb :'/users/edit'
-  else
- flash[:error]= "You cannot edit another users Artistree!"
- redirect'/users/show'
-  end
-end
+  
+  get '/signup' do
+    erb :'/users/signup'
+  end 
+  
+  post '/signup' do
+    @user = User.new(params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect"/users/#{@user.id}"
+    else
+      flash[:error]="Please fill out all fields to make an Artistree !"
+      redirect '/signup'
+    end
+  end 
+  
 
 get '/logout' do
   session.clear
